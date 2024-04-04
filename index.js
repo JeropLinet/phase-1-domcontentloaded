975c2ff04bee1d4b53fdf6b00f85dda4c0733c12
Linet <teweilinet@gmail.com>
2024-04-04 12:29:19 +0300

    dom content loaded

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+ const newText= document.getElementById("text")
+ text.innerHTML=
+ "<p>This is really cool!</p>"
+
+})
+
+    
